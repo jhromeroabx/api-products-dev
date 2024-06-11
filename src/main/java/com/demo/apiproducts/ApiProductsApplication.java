a7d06770dfb28e1667225058078aa29d20193fc2
bryan <bryanlinks@hotmail.com>
2024-06-11 02:34:59 +0200

    microservicio correcion (agregar Lombok annotation processor y package)

--- src/main/java/com/demo/apiproducts/ApiProductsApplication.java
+++ src/main/java/com/demo/apiproducts/ApiProductsApplication.java
@@ -1,4 +1,4 @@
-package com.demo.approducts;
+package com.demo.apiproducts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
