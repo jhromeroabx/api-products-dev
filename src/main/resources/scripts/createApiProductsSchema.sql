18940bb55ff8306926b7271163f9ca23711ed441
Marcos Bueno <inboxg.marcos@gmail.com>
2024-06-10 23:12:59 -0300

    Se agregar scripts createApiProductsSchema.sql

---
+++ src/main/resources/scripts/createApiProductsSchema.sql
@@ -0,0 +1,38 @@
+create schema rl_api_products;
+set
+search_path
+to rl_api_products;
+
+
+create table rl_product_type
+(
+    id_rl_product_type serial
+        primary key,
+    description        varchar not null,
+    deleted_at         timestamp
+);
+
+create table rl_product
+(
+    id_rl_product      serial
+        primary key,
+    name               varchar not null,
+    description        varchar not null,
+    large_descripcion  text not null,
+    deleted_at         timestamp,
+    id_rl_product_type integer not null
+        constraint fk_product_type
+            references rl_product_type (id_rl_product_type)
+);
+
+create table rl_product_image
+(
+    id_rl_product_image serial
+        primary key,
+    id_rl_product       integer not null
+        references rl_product (id_rl_product),
+    provider            varchar not null,
+    provider_link       varchar not null,
+    deleted_at          timestamp
+);
+
