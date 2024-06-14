0f73d0e189b9b20c3480230635b80c1e5ddc04ce
Marcos Bueno <inboxg.marcos@gmail.com>
2024-06-13 21:10:24 -0300

    Se corrige PR, quita salto de linea

--- src/main/java/com/demo/apiproducts/ApiProductsApplication.java
+++ src/main/java/com/demo/apiproducts/ApiProductsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiProductsApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(ApiProductsApplication.class, args);
   }
-
 }
