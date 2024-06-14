1085f9e776a9b6eb6028aa49e04a4bbd9300dfc8
Marcos Bueno <inboxg.marcos@gmail.com>
2024-06-13 21:08:04 -0300

    Se corrige PR

--- src/main/java/com/demo/apiproducts/model/RlProductImage.java
+++ src/main/java/com/demo/apiproducts/model/RlProductImage.java
@@ -1,13 +1,6 @@
 package com.demo.apiproducts.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,6 +9,7 @@ import java.util.Date;
 @Getter
 @Setter
 @Entity
+@Table(name = "rl_product_image")
 public class RlProductImage {
 
     @Id
