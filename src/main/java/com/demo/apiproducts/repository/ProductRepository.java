4ed401e5e7d65631a742335f83de2d3aea413289
Marcos Bueno <inboxg.marcos@gmail.com>
2024-07-01 17:27:02 -0300

    Correcciones RLA14

--- src/main/java/com/demo/apiproducts/repository/ProductRepository.java
+++ src/main/java/com/demo/apiproducts/repository/ProductRepository.java
@@ -4,8 +4,10 @@ import com.demo.apiproducts.model.RlProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.Query;
 
+@Transactional
 @Repository
 public interface ProductRepository extends JpaRepository <RlProduct, Long> {
 
