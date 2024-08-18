124d1481818391c7dd34f7a6300393289036368a
Jhosep Adbel Romero Loa <jhromero.abx@gmail.com>
2024-08-17 22:53:31 -0500

    add swagger auth bearer jwt

---
+++ src/main/java/com/demo/apiproducts/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.demo.apiproducts.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(Arrays.asList(
+                        new Server().url("http://localhost:8080").description("API Products Dev Server"),
+                        new Server().url("https://api-products-dev.abexa.pe").description("API Products Deployed Server")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .name("bearerAuth")
+                                .type(Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
