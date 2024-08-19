d515dd33f890ccd7dd4db8787a52970a1f234294
Jhosep Adbel Romero Loa <jhromero.abx@gmail.com>
2024-08-18 20:10:49 -0500

    refix sql struct && dotenv

--- src/main/resources/scripts/alterTableRlProduct.sql
+++ src/main/resources/scripts/alterTableRlProduct.sql
@@ -1,4 +1,8 @@
 ALTER TABLE rl_api_products.rl_product
-    ADD COLUMN currency VARCHAR(10) NOT NULL,
-    ADD COLUMN price DECIMAL(10, 2) NOT NULL,
+    ADD COLUMN currency VARCHAR(10) NOT NULL;
+
+ALTER TABLE rl_api_products.rl_product
+    ADD COLUMN price DECIMAL(10, 2) NOT NULL;
+
+ALTER TABLE rl_api_products.rl_product
     RENAME COLUMN large_descripcion TO large_description;
